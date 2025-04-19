d0203c05741e194b40e7dcfc94b551ebd5ced0aa
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-04-19 16:09:46 +0100

    drop column client_id && client_name from reservations table

---
+++ database/migrations/2025_04_19_150058_drop_client_id_and_client_name_from_reservations.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            $table->dropForeign(['client_id']);
+            $table->dropColumn(['client_id', 'client_name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            $table->foreignId('client_id')
+                ->nullable()
+                ->constrained('users')
+                ->onDelete('set null')
+                ->after('sportive_id');
+
+            $table->string('client_name')
+                ->nullable()
+                ->after('client_id');
+        });
+    }
+};
