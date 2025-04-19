81c2c01e733c1ce87b26aeb6b7441cd58ba3a8d1
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-04-19 15:31:25 +0100

    add link to form the reservation in back office

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -64,10 +64,10 @@
             <p class="px-4 text-xs font-semibold text-gray-500 uppercase tracking-wider">Opérations</p>
         </div>
 
-        <a href="{{ route('dashboard.availabilities') }}" class="block px-4 py-2 {{ request()->routeIs('dashboard.availabilities') ? 'bg-emerald-100 text-emerald-700 border-l-4 border-emerald-600' : 'text-gray-700 hover:bg-gray-100' }}">
+        <a href="{{ route('dashboard.createreservation') }}" class="block px-4 py-2 {{ request()->routeIs('dashboard.createreservation') ? 'bg-emerald-100 text-emerald-700 border-l-4 border-emerald-600' : 'text-gray-700 hover:bg-gray-100' }}">
             <div class="flex items-center">
                 <i class="fas fa-calendar-alt mr-3"></i>
-                <span>Disponibilités</span>
+                <span>Créer une Réservation </span>
             </div>
         </a>
 
