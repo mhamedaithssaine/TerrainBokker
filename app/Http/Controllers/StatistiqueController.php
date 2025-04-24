08f935cf60c8ceb15980145f17a062f0a5dd2c8e
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-04-24 10:24:18 +0100

    add payment statique, diplay for admin revenusMois

--- app/Http/Controllers/StatistiqueController.php
+++ app/Http/Controllers/StatistiqueController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use App\Models\User;
+use App\Models\Payment;
 use App\Models\Feedback;
 use App\Models\Reservation;
 use Illuminate\Http\Request;
@@ -12,19 +13,35 @@ class StatistiqueController extends Controller
 {
     public function index()
     {
+
+        $reservations = Reservation::with(['terrain', 'utilisateur', 'payment'])
+            ->latest()
+            ->take(10)
+            ->get();
+
+
         $feedbacks = Feedback::with('user')
                              ->where('status', 'cacher')
                              ->orderBy('created_at', 'desc')
                              ->take(3)
                              ->get()
                              ->map(function ($feedback) {
-                                 $feedback->date = \Carbon\Carbon::parse($feedback->created_at)->diffForHumans();
+                                 $feedback->date = Carbon::parse($feedback->created_at)->diffForHumans();
                                  return $feedback;
                              });
 
-        return view('components.feedbackrecents', compact('feedbacks'));
+        return view('components.feedbackrecents', compact('feedbacks','reservations'));
 }
 
+public function recentReservations()
+    {
+        $reservations = Reservation::with(['terrain', 'utilisateur', 'payment'])
+            ->latest()
+            ->take(10)
+            ->get();
+
+        return view('components.recent-reservations-table', compact('reservations'));
+    }
 
     public function StatistiqueCard(){
                 
@@ -36,8 +53,15 @@ class StatistiqueController extends Controller
             $nouveauxClients = User::whereMonth('created_at', Carbon::now()->month)
                 ->whereYear('created_at', Carbon::now()->year)
                 ->count();
+            
+                $revenusMois = Payment::whereHas('reservation', function ($query) {
+                    $query->whereMonth('date_debut', Carbon::now()->month)
+                          ->whereYear('date_debut', Carbon::now()->year);
+                })
+                ->where('status', 'success') 
+                ->sum('amount');
 
-            return view('components.stats-card', compact( 'reservationsToday', 'noteMoyenne','nouveauxClients'
+            return view('components.stats-card', compact( 'reservationsToday', 'noteMoyenne','nouveauxClients','revenusMois'
             ));
     }
 
