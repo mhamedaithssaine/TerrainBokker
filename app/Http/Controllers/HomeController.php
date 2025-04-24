7221ad89146f7b1be7762c7f62cce67c2c451da7
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-04-24 09:55:08 +0100

    update index methode for display the terrains reserve between date debut && now

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,35 +17,58 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
+
         $categories = Category::all();
-
+    
+        
         $query = Terrain::with('categorie');
-
+    
+     
         if ($request->filled('search')) {
             $search = $request->input('search');
+    
             $query->where(function ($q) use ($search) {
                 $q->where('name', 'like', "%{$search}%")
                   ->orWhere('adresse', 'like', "%{$search}%");
             });
         }
-
+    
         if ($request->filled('categorie')) {
+           
             $query->where('categorie_id', $request->input('categorie'));
         }
-
+    
+ 
         if ($request->filled('prix_max')) {
+      
             $query->where('prix', '<=', $request->input('prix_max'));
         }
-
+    
+        
         $terrains = $query->paginate(3);
-
-        $reservations = Reservation::whereIn('terrain_id', $terrains->pluck('id'))
-            ->where('date_fin', '>=', Carbon::now())
-            ->get(['terrain_id', 'date_debut', 'date_fin'])
-            ->groupBy('terrain_id');
-
-        $feedbacks = Feedback::where('status', 'publier')->with('user')->latest()->get();
-        return view ('home',compact('terrains','reservations','categories','feedbacks'));
+    
+ 
+        $terrainIds = $terrains->pluck('id');
+    
+       
+        $reservations = Reservation::whereIn('terrain_id', $terrainIds) 
+            ->where('date_debut', '>', Carbon::now()) 
+            ->where('statut', 'confirmée') 
+            ->where('payment_status', 'payé') 
+            ->get(['id', 'terrain_id', 'date_debut', 'date_fin']); 
+    
+       
+        $reservationsByTerrain = $reservations->groupBy('terrain_id');
+    
+       
+        $feedbacks = Feedback::where('status', 'publier') 
+            ->with('user') 
+            ->latest() 
+            ->get(); 
+    
+     
+        return view('home', compact('terrains', 'reservations', 'categories', 'feedbacks'))
+            ->with('reservations', $reservationsByTerrain);
 
         
     }
