2aa75d80457d592d3d48b2ec536c680132390a3f
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-04-24 10:28:10 +0100

    add reservations relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -81,4 +81,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(Feedback::class);
     }
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class, 'sportive_id');
+    }
 }
