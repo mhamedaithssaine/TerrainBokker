a1dad0dda2c622620d3e76922904cf35be53c715
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-04-24 22:08:39 +0100

    style: update success message styling with custom green background and border

--- resources/views/permissions/index.blade.php
+++ resources/views/permissions/index.blade.php
@@ -4,17 +4,16 @@
     <h1 class="text-2xl font-semibold text-gray-800 mb-6">Gestion des Permission</h1>
 
     <div class="bg-white rounded-lg shadow p-6">
-        <a href="{{ route('permissions.create') }}" class="inline-flex items-center px-4 py-2 bg-emerald-600 border border-transparent rounded-md font-semibold text-white hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500">
+        <a href="{{ route('permissions.create') }}" class="mb-4 inline-flex items-center px-4 py-2 bg-emerald-600 border border-transparent rounded-md font-semibold text-white hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500">
             <i class="fas fa-plus mr-2"></i>
            Permission
         </a>
         
         @if (session('success'))
-            <div class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
-                <p class="font-bold">Informational message</p>
-                <p class="text-sm">{{ session('success') }}</p>
-              </div>
-        @endif
+        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4" role="alert">
+            <span class="block sm:inline">{{ session('success') }}</span>
+        </div>
+    @endif
   
       
 
