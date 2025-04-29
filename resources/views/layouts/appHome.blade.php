5515687e317569bcb1cc7b65531c470b0f518882
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-04-29 10:58:03 +0100

    feat(layout-appHome): include Vite assets in appHome layout for CSS and JS

--- resources/views/layouts/appHome.blade.php
+++ resources/views/layouts/appHome.blade.php
@@ -9,7 +9,7 @@
       <link href="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.css" rel="stylesheet">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.min.css">
 
-
+      @vite(['resources/css/app.css', 'resources/js/app.js']);
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
        <!-- Include Leaflet CSS -->
@@ -18,6 +18,8 @@
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 
     <script src="https://cdn.tailwindcss.com"></script>
+
+  
     <script>
         tailwind.config = {
             theme: {
