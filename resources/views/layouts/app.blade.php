c42357bfc942466a60d7d6c044417f31cea23d4e
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-05-05 00:03:53 +0100

    chore(app-auth): include Vite assets for styling and JS support

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'TerrainBokker')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js']);
+
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
