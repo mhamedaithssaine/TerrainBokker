8f99247975dae93e4ee72cdd5db8fad9017e1043
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-05-04 23:56:05 +0100

    refactor(tags): replace Tag::all() with pagination using Tag::paginate(5)

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -14,7 +14,7 @@ class TagController extends Controller
      */
     public function index()
     {
-        $tags = Tag::all();
+        $tags = Tag::paginate(5);
         return view('tags.index', compact('tags'));
     }
 
